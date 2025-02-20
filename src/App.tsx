e9fb55e64a200e778738071f0b7a8e46bb8f9354
Nathaniel <nathanielmak@outlook.com>
2025-02-20 13:04:09 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
 
-function App() {
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([])
+  
+  const handleAddNote = (title: string, content: string) => {
+    setNotes([...notes, { id: Date.now(), title, content }])
+  }
+
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="app-container">
+        <form className="note-form">
+          <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <textarea placeholder="Content" rows={10} required value={content} onChange={(e) => setContent(e.target.value)} />
+          <button type="submit">Add Note</button>
+        </form>
+      </div>
+      <div className="notes-container">
+        {notes.map((note, index) => (
+          <div className="note-item" key={index}>
+            <h3>{note.title}</h3>
+            <p>{note.content}</p>
+            <button>x</button>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default App;
